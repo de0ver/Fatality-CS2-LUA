1720250e809b71301b209583e58f814f0a3e7b5f
de0ver <kiduaclub@gmail.com>
2024-12-30 18:38:27 +0300

    Release

---
+++ NotifyLogs.lua
@@ -0,0 +1,163 @@
+local logs = gui.checkbox(gui.control_id('enable_logs'));
+-- local debug_logs = gui.checkbox(gui.control_id('enable_debug'));
+local row = gui.make_control('Notify Logs', logs);
+-- local test = gui.make_control('Debug Logs', debug_logs);
+local group = gui.ctx:find('lua>elements a');
+-- local group_b = gui.ctx:find('lua>elements b');
+group:add(row);
+-- group_b:add(test);
+
+local w, h = game.engine:get_screen_size();
+local hitgroup = {
+	[0] = 'generic',
+	[1] = 'head',
+	[2] = 'chest',
+	[3] = 'stomach',
+	[4] = 'left arm',
+	[5] = 'right arm',
+	[6] = 'left leg',
+	[7] = 'right leg'
+};
+local weapon_name = {
+	['ak47'] = 'AK-47',
+    ['aug'] = 'AUG',
+    ['awp'] = 'AWP',
+    ['bizon'] = 'PP-Bizon',
+    ['c4'] = 'C4',
+    ['cz75a'] = 'CZ75-Auto',
+    ['deagle'] = 'Desert Eagle',
+    ['decoy'] = 'Decoy',
+    ['elite'] = 'Dual Berettas',
+    ['famas'] = 'FAMAS',
+    ['fists'] = 'Bare Hands',
+    ['fiveseven'] = 'Five-SeveN',
+    ['flashbang'] = 'Flashbang',
+    ['g3sg1'] = 'G3SG1',
+    ['galilar'] = 'Galil AR',
+    ['glock'] = 'Glock-18',
+    ['hammer'] = 'Hammer',
+    ['healthshot'] = 'Healthshot',
+    ['hkp2000'] = 'P2000',
+    ['incgrenade'] = 'Molotov',
+    ['knife'] = 'Knife',
+    ['m249'] = 'M249',
+    ['m4a1'] = 'M4A4',
+    ['m4a1_silencer'] = 'M4A1-S',
+    ['mac10'] = 'MAC-10',
+    ['mag7'] = 'MAG-7',
+    ['molotov'] = 'Molotov',
+    ['mp5sd'] = 'MP5-SD',
+    ['mp7'] = 'MP7',
+    ['mp9'] = 'MP9',
+    ['negev'] = 'Negev',
+    ['nova'] = 'Nova',
+    ['p250'] = 'P250',
+    ['p90'] = 'P90',
+    ['revolver'] = 'R8 Revolver',
+    ['sawedoff'] = 'Sawed-Off',
+    ['scar20'] = 'SCAR-20',
+    ['sg556'] = 'SG 553',
+    ['smokegrenade'] = 'Smoke',
+    ['ssg08'] = 'SSG 08',
+    ['taser'] = 'Zeus',
+    ['tec9'] = 'Tec-9',
+    ['ump45'] = 'UMP-45',
+    ['usp silencer'] = 'USP-S',
+    ['xm1014'] = 'XM1014',
+	['hegrenade'] = 'HE Grenade',
+	['inferno'] = 'Fire'
+};
+local bomb_events = {
+	['bomb_beginplant'] = 'Begin planting the bomb',
+	['bomb_abortplant'] = 'Cancelled planting the bomb',
+	['bomb_planted'] = 'Planted the bomb',
+	['bomb_defused'] = 'Defused the bomb',
+	['bomb_exploded'] = 'Exploded the bomb'
+};
+local bombsite = {
+	[0] = 'A',
+	[1] = 'B'
+};
+
+local function bomb_logs(event)
+	print('<'..
+		event:get_controller('userid'):get_name()..
+		'>'..
+		bomb_events[event:get_name()]);
+	
+	return gui.notify:add(gui.notification(
+		'<'..
+		event:get_controller('userid'):get_name()..
+		'>', 
+		bomb_events[event:get_name()], 
+		draw.textures['icon_visuals']));
+end
+
+local function hit_logs(event)
+	local message = 'Hit <'
+		..event:get_controller('userid'):get_name()..
+		'> for '
+		..event:get_int('dmg_health')..
+		' ('
+		..event:get_int('health')..
+		') hp in '
+		..hitgroup[event:get_int('hitgroup')]..
+		' with '
+		..weapon_name[event:get_string('weapon')];
+		
+	print(message);
+		
+	return gui.notify:add(gui.notification(
+		'Hit!',
+		message,
+		draw.textures['icon_rage']));
+end
+
+local function hurt_logs(event)
+	local message = 'Hurt by <'
+		..event:get_controller('attacker'):get_name()..
+		'> for '
+		..event:get_int('dmg_health')..
+		' ('
+		..event:get_int('health')..
+		') hp in '
+		..hitgroup[event:get_int('hitgroup')]..
+		' with '
+		..weapon_name[event:get_string('weapon')];
+		
+	print(message);
+	
+	return gui.notify:add(gui.notification(
+		'Hurt!', 
+		message, 
+		draw.textures['icon_legit']));
+end
+
+local function round_logs(event)
+	return gui.notify:add(gui.notification(
+		'Round started!', 
+		'Buy weapons and FIGHT!!!', 
+		draw.textures['icon_visuals']));
+end
+
+local function on_event(event)
+	if logs:get_value():get() then
+		if event:get_name() == 'player_hurt' then
+			if event:get_controller('attacker') == entities.get_local_controller() then
+				return hit_logs(event);
+			elseif event:get_controller('userid') == entities.get_local_controller() then
+				return hurt_logs(event);
+			end
+		end
+		
+		if event:get_name() == 'round_start' then
+			return round_logs(event);
+		end
+		 
+		if string.find(event:get_name(), 'bomb_') then
+			return bomb_logs(event);
+		end
+	end
+end
+
+events.event:add(on_event);
